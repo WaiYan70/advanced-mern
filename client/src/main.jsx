4a7b323712b18ea8ffc2eded6dcc50c8edd08657
Khant Wai Yan <52145576+WaiYan70@users.noreply.github.com>
2024-05-19 22:15:25 +0630

    setting up React Redux with redux tookit and adding slices for user api endpoint

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,11 @@ import {
   Route, 
   RouterProvider 
 } from 'react-router-dom'
+import store from './store.js'
+import { Provider } from 'react-redux'
+
 import App from './App.jsx'
+
 import HomePage from './pages/HomePage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
 import RegisterPage from './pages/RegisterPage.jsx'
@@ -27,7 +31,9 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  </Provider>
 )
---
+++ client/src/slices/authSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// to check local storage 
+const initialState = {
+    userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem) : null
+};
+
+/* 
+    Two Functions :  
+    1. setting credenetials which set user info to local storage 
+    2. LogOut which is going to take info out of local storage
+*/
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        setCredential: (state, action) => {
+            state.userInfo = action.payload;
+            localStorage.setItem('userInfo', JSON.stringify(action.payload));
+        },
+        logout: (state) => {
+            state.userInfo = null;
+            localStorage.removeItem('useInfo');
+        }
+    }
+})
+
+export const { setCredential, logout } = authSlice.actions;
+export default authSlice.reducer;
